93cd52d936d02d69f8b0d5f13de0e7f29d25ad5c
fdsfasdf2314 <gg.suii999@gmail.com>
2025-05-19 14:13:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,2 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const OFFLINE_URLS = [
-  '/',
-  '/index.html',
-  '/1/',
-  '/2/',
-  '/3/',
-  '/4/',
-  '/5/',
-];
-
-// Cache all files in folders 1-5 (recursive approach)
-const FOLDER_PATHS = ['1', '2', '3', '4', '5'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(async cache => {
-      await cache.addAll(OFFLINE
+// No Service Worker
+console.log("No Service Worker")
